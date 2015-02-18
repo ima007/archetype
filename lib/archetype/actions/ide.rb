6c59da3e0b49509664e4283fdf2b7f9e4f672e99
Shane Afsar <safsar@linkedin.com>
2015-02-17 16:40:06 -0800

    Add archetype "ide" command. Right now, it only generates a sublime completions file for components and icons. Additionally, it only does it for the default archetype theme. Next steps: *Get feedback on current appraoch. *Update to accept any theme.

---
+++ lib/archetype/actions/ide.rb
@@ -0,0 +1,93 @@
+description = "Generate IDE code snippets for an archetype theme"
+
+require "sass"
+require 'sass/plugin'
+require "compass"
+require "archetype"
+require "archetype-theme"
+require "compass-import-once"
+
+
+if @description.nil?
+  options = {
+    :theme => '',
+    :output => ''
+  }
+  OptionParser.new do |opts|
+    opts.banner = description
+    opts.define_head "Usage: #{Archetype.name} ide [theme] [output]"
+    opts.separator ""
+    opts.separator "Example usage:"
+    opts.separator " #{Archetype.name} ide --theme=archetype --output=/path/to/output/directory"
+    opts.separator " #{Archetype.name} ide --theme=/path/to/theme/ --output=/path/to/output/directory"
+
+    opts.on('-t', '--theme THEME', 'theme path') do |v|
+      options[:theme] = v
+    end
+
+    opts.on('-o', '--output PATH', 'folder that will contain the snippets') do |v|
+      options[:output] = v
+    end
+
+    opts.on('-h', '--help', 'shows this help message') do
+      puts opts
+      exit
+    end
+
+    if not @help.nil?
+      puts opts
+      exit
+    end
+  end.parse!
+
+  if not options[:theme].nil?
+    base = ARGV[1] || '.'
+    tmp = '/tmp/ide_' + rand(36**8).to_s(36)
+    theme_template = File.join(File.dirname(__FILE__), '../../../templates/_ide/')
+    theme_path = options[:theme]
+    output = options[:output]
+    theme_name = File.basename(options[:theme])
+    # copy template files to tmp dir
+    FileUtils.mkdir_p(tmp)
+    FileUtils.cp_r(Dir["#{theme_template}/**"], tmp)
+
+    puts "Creating sublime autocompletions for archetype to #{File.expand_path(output)}..."
+
+    if theme_path == 'archetype' || theme_path == 'default'
+    theme_name = 'archetype'
+    # compile and grab all component & variants, icons from theme
+    Dir.glob("#{tmp}/**/*.scss") do |filename|
+      base_name = File.basename(filename, ".*")
+      namespaced_name = "#{theme_name}-#{base_name}"
+      contents = []
+      css_name = filename + '.css'
+      css_output = css_name
+      Compass.compiler.compile(filename, css_output)
+      out = File.read(css_output).gsub(/\/\*/,'').gsub(/\*\//,'')
+      File.open(css_output, "w") { |file| file.puts out }
+      contents = IO.readlines css_output
+
+      #create each sublime snippet line
+      formatted_contents = []
+      contents.each { |component|
+        formatted_contents.push "{\"trigger\": \"#{component} \\t #{namespaced_name}\", \"contents\": \"#{component}\" }"
+      }
+
+      all_content = ["{\"source\": \"source.scss, meta.property-value.scss\",\"completions\":[",formatted_contents.join(","),"]}"]
+
+      # create completions file _<theme>.scss ...
+      File.open(output + "/#{namespaced_name}.sublime-completions", "w") { |file| file.puts all_content.join }
+    end
+
+    # remove tmp dir
+    FileUtils.rm_rf(tmp)
+    puts "Congratulations! Your code snippets have been created!"
+    puts "Run this periodically to get the latest components."
+    exit
+  end
+else
+    @description = "Sorry, only the default archetype theme is supported right now."
+end
+else
+  @description = description
+end
